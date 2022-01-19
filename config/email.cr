181c7d0d3d1513e598f75cf613fc1e7c54b990c3
akadusei <attakusiadusei@gmail.com>
2022-01-19 22:38:14 +0000

    Break long line into multiple lines

--- config/email.cr
+++ config/email.cr
@@ -19,6 +19,8 @@ private def send_grid_key
 end
 
 private def raise_missing_key_message
-  puts "Missing SEND_GRID_KEY. Set the SEND_GRID_KEY env variable to 'unused' if not sending emails, or set the SEND_GRID_KEY ENV var.".colorize.red
+  puts "Missing SEND_GRID_KEY. Set the SEND_GRID_KEY env variable to 'unused' \
+    if not sending emails, or set the SEND_GRID_KEY ENV var.".colorize.red
+
   exit(1)
 end
