d544393a8e988d908871ee1bda15cd8a6b520efe
akadusei <attakusiadusei@gmail.com>
2022-01-21 22:40:45 +0000

    Add first name and last name defaults to `UserFactory`

--- spec/support/factories/user_factory.cr
+++ spec/support/factories/user_factory.cr
@@ -8,6 +8,8 @@ class UserFactory < Avram::Factory
   end
 
   private def set_defaults
+    first_name "Mary"
+    last_name "Smith"
     email "user@domain.tld"
     level UserLevel.new(:author)
     password "password12U~password"
