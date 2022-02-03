a748bd6f5fca0f6bac6d8f4449342059899d232b
akadusei <attakusiadusei@gmail.com>
2022-02-03 15:43:03 +0000

    Add missing `level` attribute for `UserSerializer`

--- src/serializers/user_serializer.cr
+++ src/serializers/user_serializer.cr
@@ -6,6 +6,7 @@ struct UserSerializer < BaseSerializer
     {
       id: @user.id,
       email: @user.email,
+      level: @user.level,
       first_name: @user.first_name,
       last_name: @user.last_name
     }
