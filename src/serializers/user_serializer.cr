7b40f3bc5197d62f047f0ec1e27216f9091634dd
akadusei <attakusiadusei@gmail.com>
2022-02-23 11:58:54 +0000

    Add missing user serializer attributes

--- src/serializers/user_serializer.cr
+++ src/serializers/user_serializer.cr
@@ -8,7 +8,9 @@ struct UserSerializer < BaseSerializer
       email: @user.email,
       level: @user.level,
       first_name: @user.first_name,
-      last_name: @user.last_name
+      last_name: @user.last_name,
+      created_at: @user.created_at.to_unix,
+      updated_at: @user.updated_at.to_unix,
     }
   end
 end
