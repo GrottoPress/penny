44d99f24fe05e57e932e652a209e1745ff57c521
akadusei <attakusiadusei@gmail.com>
2022-04-20 14:43:20 +0000

    Add more pagination attributes
    The idea here is to free the frontend from having to calculate
    pagination parameters itself. We are therefore sending along the
    pagination series as built by *Lucky*.
    
    Here, a negative number represents the current page, a `0`
    represents a gap. Eg: `[1,0,5,-6,7,0,11]`.
    
    See <https://github.com/luckyframework/lucky/blob/91864a69ceea20430f63b93171a30ff28fbf32fc/src/lucky/paginator/paginator.cr#L113>.

--- src/serializers/success_serializer.cr
+++ src/serializers/success_serializer.cr
@@ -7,7 +7,15 @@ abstract struct SuccessSerializer < BaseSerializer
     @pages.try do |pages|
       json = json.merge({pages: {
         current: pages.page,
-        per_page: pages.per_page,
+        items: {
+          count: pages.per_page,
+          end: pages.item_range.end,
+          start: pages.item_range.begin,
+          total: pages.item_count,
+        },
+        next: pages.next_page,
+        previous: pages.previous_page,
+        series: pages_series(pages),
         total: pages.total,
       }})
     end
@@ -21,5 +29,29 @@ abstract struct SuccessSerializer < BaseSerializer
 
   private def status : Status
     Status::Success
+  end
+
+  # The idea here is to free the frontend from having to calculate
+  # pagination parameters itself. We are therefore sending along the
+  # pagination series as built by *Lucky*.
+
+  # Here, a negative number represents the current page, a `0`
+  # represents a gap. Eg: `[1,0,5,-6,7,0,11]`.
+  private def pages_series(pages)
+    pages.series(
+      begin: 1,
+      end: 1,
+      left_of_current: 2,
+      right_of_current: 2
+    ).map do |item|
+      case item
+      when Lucky::Paginator::CurrentPage
+        -item.number
+      when Lucky::Paginator::Page
+        item.number
+      else
+        0
+      end
+    end
   end
 end
