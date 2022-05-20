382de3dff9fb87827070c9a304d157bb2af40784
akadusei <attakusiadusei@gmail.com>
2022-05-20 13:23:03 +0000

    Refactor `RegisterCurrentUser#set_level`
    Make it read more intuitively, and performant.

--- src/operations/register_current_user.cr
+++ src/operations/register_current_user.cr
@@ -10,10 +10,10 @@ class RegisterCurrentUser < User::SaveOperation
   include Shield::SendWelcomeEmail
 
   private def set_level
-    if !App.settings.make_first_user_admin || UserQuery.new.first?
-      level.value = UserLevel.new(:author)
-    else
+    if App.settings.make_first_user_admin && UserQuery.new.none?
       level.value = UserLevel.new(:admin)
+    else
+      level.value = UserLevel.new(:author)
     end
   end
 end
