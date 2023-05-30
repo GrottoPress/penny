9dde5b4cab4d771a942f8e9e6e8a42fb36446330
akadusei <attakusiadusei@gmail.com>
2023-05-30 22:46:03 +0000

    Integrate `GrottoPress/halo` shard
    See <https://github.com/grottoPress/halo>.
    
    Replaces `luckyframework/carbon_sendgrid_adapter`.

--- config/email.cr
+++ config/email.cr
@@ -2,25 +2,14 @@ require "lucille/spec"
 
 BaseEmail.configure do |settings|
   if LuckyEnv.production?
-    # If you don't need to send emails, set the adapter to DevAdapter instead:
-    #
-    #   settings.adapter = Carbon::DevAdapter.new
-    #
-    # If you do need emails, get a key from SendGrid and set an ENV variable
-    settings.adapter = Carbon::SendGridAdapter.new(api_key: send_grid_key)
+    settings.adapter = Halo::Adapter.new(
+      App.settings.email_from,
+      ENV["SMTP_PASSWORD"],
+      ENV["SMTP_HOST"],
+      ENV["SMTP_PORT"].to_i
+    )
   else
     settings.adapter = Carbon::DevAdapter.new
     settings.deliver_later_strategy = DevDeliverLaterStrategy.new
   end
-end
-
-private def send_grid_key
-  ENV["SEND_GRID_KEY"]? || raise_missing_key_message
-end
-
-private def raise_missing_key_message
-  puts "Missing SEND_GRID_KEY. Set the SEND_GRID_KEY env variable to 'unused' \
-    if not sending emails, or set the SEND_GRID_KEY ENV var.".colorize.red
-
-  exit(1)
 end
