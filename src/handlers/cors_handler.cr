37d2af7d7d548e0f0ab86f25ab1ba00881140734
akadusei <attakusiadusei@gmail.com>
2023-08-05 20:17:35 +0000

    Redo CORS handling

--- src/handlers/cors_handler.cr
+++ src/handlers/cors_handler.cr
@@ -10,41 +10,40 @@ class CorsHandler < BaseHandler
   end
 
   private def set_generic_headers(context)
-    response = context.response
+    request, response = context.request, context.response
 
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = allow_methods
-    response.headers["Access-Control-Allow-Headers"] = allow_headers
+    response.headers["Access-Control-Allow-Origin"] = allow_origin(request)
+    response.headers.add("Vary", "Origin")
   end
 
   private def set_options_headers(context)
-    response = context.response
+    request, response = context.request, context.response
 
     response.status = HTTP::Status::NO_CONTENT
+    response.headers["Access-Control-Allow-Methods"] = allow_methods
+    response.headers["Access-Control-Allow-Headers"] = allow_headers(request)
     response.headers["Access-Control-Max-Age"] = max_age
     response.headers["Content-Type"] = "text/plain"
     response.headers["Content-Length"] = "0"
   end
 
   private def allow_methods
-    {"DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST"}.join(',')
+    {"DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST"}.join(", ")
   end
 
-  private def allow_headers
-    {
-      "Authorization",
-      "Cache-Control",
-      "Content-Type",
-      "DNT",
-      "If-Modified-Since",
-      "Range",
-      "User-Agent",
-      "X-Forwarded-For",
-      "X-Requested-With"
-    }.join(',')
+  private def allow_headers(request)
+    request.headers["Access-Control-Request-Headers"]? || ""
   end
 
   private def max_age
-    "#{7.days.total_seconds.to_i}"
+    "#{1.hour.total_seconds.to_i}"
+  end
+
+  private def allow_origin(request)
+    "*"
+
+    # You may use this, instead, for API backends
+    # origin = request.headers["Origin"]?
+    # App.settings.frontend_url == origin ? origin.to_s : ""
   end
 end
