a0bd8f577b0f430b23b701c1f5be901e6d1b74b2
akadusei <attakusiadusei@gmail.com>
2024-06-07 08:50:00 +0000

    Add missing worker tasks to `Procfile`

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 release: ./bin/cli db.migrate
 web: ./bin/app
+worker_1: ./bin/worker 1
+worker_2: ./bin/worker 2
--- src/worker.cr
+++ src/worker.cr
@@ -14,4 +14,6 @@ DeleteEmailConfirmationsJob.run_every(
 DeleteLoginsJob.run_every(1.week, id: "delete-logins-weekly")
 DeletePasswordResetsJob.run_every(1.week, id: "delete-password-resets-weekly")
 
+ARGV.first?.try { |worker_id| Mel.settings.worker_id = worker_id.to_i }
+
 Mel.start
