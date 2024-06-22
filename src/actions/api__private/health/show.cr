f7bc5e278f67534ba8b310006947c97d60532c46
akadusei <attakusiadusei@gmail.com>
2024-06-22 16:59:30 +0000

    Check database and redis connections during health check

--- src/actions/api__private/health/show.cr
+++ src/actions/api__private/health/show.cr
@@ -3,6 +3,17 @@
 #   skip :require_logged_out
 
 #   get "/up" do
+#     verify_database_connection
+#     verify_redis_connection
+
 #     head :no_content
+#   end
+
+#   private def verify_database_connection
+#     Db::VerifyConnection.new(quiet: LuckyEnv.production?).run_task
+#   end
+
+#   private def verify_redis_connection
+#     Mel.redis.run({"PING"})
 #   end
 # end
