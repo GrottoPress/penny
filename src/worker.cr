53fce2a1086a70c4dacc38f1e7d92c7bac9b36a8
akadusei <attakusiadusei@gmail.com>
2024-07-22 14:13:28 +0000

    Schedule global jobs atomically

--- src/worker.cr
+++ src/worker.cr
@@ -6,15 +6,31 @@ Habitat.raise_if_missing_settings!
 
 exit if LuckyEnv.development?
 
-DeleteBearerLoginsJob.run_every(1.week, id: "delete-bearer-logins-weekly")
+Mel.redis.multi do |redis|
+  DeleteBearerLoginsJob.run_every(
+    1.week,
+    redis: redis,
+    id: "delete-bearer-logins-weekly"
+  )
 
-DeleteEmailConfirmationsJob.run_every(
-  1.week,
-  id: "delete-email-confirmations-weekly"
-)
+  DeleteEmailConfirmationsJob.run_every(
+    1.week,
+    redis: redis,
+    id: "delete-email-confirmations-weekly"
+  )
 
-DeleteLoginsJob.run_every(1.week, id: "delete-logins-weekly")
-DeletePasswordResetsJob.run_every(1.week, id: "delete-password-resets-weekly")
+  DeleteLoginsJob.run_every(
+    1.week,
+    redis: redis,
+    id: "delete-logins-weekly"
+  )
+
+  DeletePasswordResetsJob.run_every(
+    1.week,
+    redis: redis,
+    id: "delete-password-resets-weekly"
+  )
+end
 
 ARGV.first?.try { |worker_id| Mel.settings.worker_id = worker_id.to_i }
 
