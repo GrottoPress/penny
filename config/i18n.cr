4159b84db9866290c4b06ba31b2349438e498618
akadusei <attakusiadusei@gmail.com>
2025-02-16 16:06:18 +0000

    Change long-lived `struct`s to `class`es

--- config/i18n.cr
+++ config/i18n.cr
@@ -1,8 +1,12 @@
+Rex.configure do |settings|
+  settings.adapter = Adapter.new
+end
+
 I18n.config.loaders << I18n::Loader::YAML.new("lang")
 I18n.config.default_locale = :en
 I18n.init
 
-private struct I18nAdapter
+private class Adapter
   include Rex::Adapter
 
   def translate(text : String | Symbol, **named_args) : String
@@ -14,8 +18,4 @@ private struct I18nAdapter
   def localize(value, *args) : String
     I18n.l(value, *args)
   end
-end
-
-Rex.configure do |settings|
-  settings.adapter = I18nAdapter.new
 end
