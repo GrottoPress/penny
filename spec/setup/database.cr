fd150a17103d6dd451c73cdde35074dc3ebf6617
akadusei <attakusiadusei@gmail.com>
2026-05-01 23:39:17 +0000

    Fix error
    ```
    Unhandled exception: The class method `database` must be defined on BaseModel, the BaseModel,
    or defined as separate `read_database` and `write_database` class methods.
    
    (Exception)
      from lib/avram/src/avram/model.cr:24:5 in 'database'
      from spec/setup/database.cr:8:3 in '->'
      # ...
    ```
    
    Regression in commit 0ccd9046851f04d4f88db0607275adb1a2ac2e7f.

--- spec/setup/database.cr
+++ spec/setup/database.cr
@@ -5,7 +5,7 @@ end
 Db::Migrate.new(quiet: true).call
 
 Spec.before_suite do
-  BaseModel.database.truncate(restart_identity: false)
+  Avram.settings.database_to_migrate.truncate(restart_identity: false)
 end
 
 Avram::SpecHelper.use_transactional_specs(Avram.settings.database_to_migrate)
